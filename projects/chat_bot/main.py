b297e37d05c65529df9aa077b2a737f48c42638c
taka-rl <takaya.murakami.uni@gmail.com>
2024-09-02 09:58:25 +0200

    created a simple chatbot

---
+++ projects/chat_bot/main.py
@@ -0,0 +1,52 @@
+from difflib import SequenceMatcher
+from datetime import datetime
+
+
+class ChatBot:
+    def __int__(self, name: str, responses: dict[str, str]) -> None:
+        self.name = name
+        self.responses = responses
+
+    @staticmethod
+    def calculate_similarity(input_sentence: str, response_sentence: str) -> float:
+        sequence: SequenceMatcher = SequenceMatcher(a=input_sentence, b=response_sentence)
+        return sequence.ration()
+
+    def get_best_response(self, user_input: str) -> tuple[str, float]:
+        highest_similarity: float = 0.0
+        best_match: str = 'Sorry, I didn\'t understand that'
+
+        for response in self.responses:
+            similarity: float = self.calculate_similarity(user_input, response)
+            if similarity > highest_similarity:
+                highest_similarity = similarity
+                best_match: str = self.responses[response]
+
+        return best_match, highest_similarity
+
+    def run(self) -> None:
+        print(f'Hello!, My name is {self.name}, how can I help you?')
+
+        while True:
+            user_input: str = input('You: ')
+            response, similarity = self.get_best_response(user_input)
+
+            if response == 'GET_TIME':
+                response = f'The time is: {datetime.now(): %H:%M}'
+            print(f'{self.name}: {response} (Similarity: {similarity:.2%}')
+
+
+def main() -> None:
+    responses: dict[str, str] = {
+        'hello': 'Hello! How are you today?',
+        'how are you': 'Great, thanks! What about you?',
+        'what time is it': 'GET_TIME',
+        'bye': 'Goodbye! Have a great day!'
+    }
+
+    chatbot: ChatBot = ChatBot(name='Taka', responses=responses)
+    chatbot.run()
+
+
+if __name__ == '__main__':
+    main()
