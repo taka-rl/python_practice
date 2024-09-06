abaac6a37dc8ce3dada9d4b443fff915a6a21f58
taka-rl <takaya.murakami.uni@gmail.com>
2024-09-06 11:24:07 +0200

    created style_decorator displayed a gif which counts between 1 and 9

--- flask_fundamentals/higher_lower/server.py
+++ flask_fundamentals/higher_lower/server.py
@@ -2,25 +2,55 @@ from flask import Flask
 import random
 
 
+INIT = "https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif"
+CORRECT = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGR5ZDA5ejh6bXJjaGF6aWE5cWtqbDg2OHBhOWo3Mng3d216OG53bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13mLwGra9bNEKQ/giphy.gif"
+LOW = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTU1NzY2Zmtnb3o5cng4ZGE1d2E1aTkzZG05NzExMTl6ZHU0bWQ1dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8Bbl0U61TN6DK/giphy.gif"
+HIGH = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXpxcXQwZHB3N3poOWwwOHc4andzY2J4cDlxMmE0N3k3Znh0MHJmaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fnlXXGImVWB0RYWWQj/giphy.gif"
+
 app = Flask(__name__)
 
 
-@app.route("/")
+def style_decorator(function):
+    def wrapper(**kwargs):
+        text = function(**kwargs)
+        if function.__name__ == "check_number":
+            if 'low' in text:
+                text_color = "pink"
+                git_link = LOW
+            elif 'high' in text:
+                text_color = "purple"
+                git_link = HIGH
+            else:
+                text_color = "blue"
+                git_link = CORRECT
+
+        else:
+            text_color = "black"
+            git_link = INIT
+
+        return f'<h1 style="color:{text_color}">{text}</h1><br><br><img src="{git_link}">'
+
+    return wrapper
+
+
+@app.route("/", endpoint='init_page')
+@style_decorator
 def init_page():
     return '<h1 style="text-align:center">Guess Number between 1 and 9!</h1>'
 
 
 @app.route("/<int:n>")
+@style_decorator
 def check_number(n: int) -> str:
     if number == n:
-        return '<h1>You found me!</h1>' \
-               '<img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGR5ZDA5ejh6bXJjaGF6aWE5cWtqbDg2OHBhOWo3Mng3d216OG53bCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/13mLwGra9bNEKQ/giphy.gif">'
+        text = 'You found me!'
+        return text
     elif number < n:
-        return '<h1>Too high, try again!</h1>' \
-               '<img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExZXpxcXQwZHB3N3poOWwwOHc4andzY2J4cDlxMmE0N3k3Znh0MHJmaiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/fnlXXGImVWB0RYWWQj/giphy.gif">'
+        text = 'Too high, try again!'
+        return text
     elif number > n:
-        return '<h1>Too low, try again!</h1>' \
-               '<img src="https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTU1NzY2Zmtnb3o5cng4ZGE1d2E1aTkzZG05NzExMTl6ZHU0bWQ1dCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8Bbl0U61TN6DK/giphy.gif">'
+        text = 'Too low, try again!'
+        return text
 
 
 if __name__ == "__main__":
