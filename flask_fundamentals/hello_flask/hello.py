cfe4096c32d2a463537523d86acbca2255c63398
taka-rl <takaya.murakami.uni@gmail.com>
2024-09-06 09:39:15 +0200

    created decorator functions to add functionalities to say_bye()

--- flask_fundamentals/hello_flask/hello.py
+++ flask_fundamentals/hello_flask/hello.py
@@ -4,12 +4,42 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper(*args, **kwargs):
+        return '<b>' + function(*args, ** kwargs) + '</b>'
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper(*args, **kwargs):
+        return '<em>' + function(*args, **kwargs) + '</em>'
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper(*args, **kwargs):
+        return '<u>' + function(*args, **kwargs) + '</u>'
+    return wrapper
+
+
+def make_h1(function):
+    def wrapper(*args, **kwargs):
+        return '<h1>' + function(*args, **kwargs) + '</h1>'
+    return wrapper
+
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return '<h1 style="text-align:center">Hello, World!</h1>' \
+           '<p>This is a paragraph.</p>' \
+           '<img src="https://www.theodysseyonline.com/media-library/image.gif?id=12859407&width=800&quality=80"width=200>'
 
 
 @app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
+@make_h1
 def say_bye():
     return "Bye"
 
