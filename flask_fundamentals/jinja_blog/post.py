9d196bcfce33eef8c8fb0ecfb9bff5aaa5abb664
taka-rl <takaya.murakami.uni@gmail.com>
2024-09-09 07:54:46 +0200

    organise the project

--- flask_fundamentals/jinja_blog/post.py
+++
@@ -1,6 +0,0 @@
-class Post:
-    def __init__(self, id_num: int, title: str, subtitle: str, body: str) -> None:
-        self.id = id_num
-        self.title = title
-        self.subtitle = subtitle
-        self.body = body
